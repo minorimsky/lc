7e1fdd4f3d93ab484c7c69e7ffc819bc55e8de6e
mionori <minorin.web@gmail.com>
2025-11-14 22:28:27 +0900

    first-commit

---
+++ js/script.js
@@ -0,0 +1,61 @@
+//header
+$(".h-open").click(function () {
+  //ボタンがクリックされたら
+  $(this).toggleClass('active');
+  //ボタン自身に activeクラスを付与する
+  $("#h-nav").toggleClass('panelactive');
+  //ナビゲーションにpanelactiveクラスを付与する
+});
+
+$("#h-nav a").click(function () {
+  //ナビゲーションのリンクがクリックされたら
+  $(".h-open").removeClass('active');
+  //ボタンの activeクラスを除去して
+  $("#h-nav").removeClass('panelactive');
+  //ナビゲーションのpanelactiveクラスも除去する
+});
+
+//interviews
+document.addEventListener('DOMContentLoaded', function () {
+
+  const mySwiper01 = new Swiper('#js-interviews-swiper',  {
+    breakpoints: {
+      767.9: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+    },
+
+    autoHeight: true,
+
+    // Optional parameters
+    loop: true,
+    loopAdditionalSlides: 1,
+    speed: 2000,
+    autoplay: {
+      delay: 7000,
+      disableOnInteraction: false,
+      waitForTransition: false,
+    },
+
+    // If we need pagination
+    pagination: {
+      el: "#js-interviews-pagination",
+      type: "bullets",
+      clickable: "clickable",
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '#js-interviews-next',
+      prevEl: '#js-interviews-prev',
+    },
+  });
+});
